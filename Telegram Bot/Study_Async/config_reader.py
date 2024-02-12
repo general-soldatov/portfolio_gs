a87f1f1354d1580bcc785737a2dc98e60e7ac8e3
Юрий Солдатов <141447194+general-soldatov@users.noreply.github.com>
2024-02-12 17:09:31 +0300

    Create config_reader.py

---
+++ Telegram Bot/Study_Async/config_reader.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+class Setting(BaseSettings):
+
+    bot_token: SecretStr
+
+    model_config = SettingsConfigDict(env_file='bot.env', env_file_encoding='utf-8')
+
+config = Setting()
