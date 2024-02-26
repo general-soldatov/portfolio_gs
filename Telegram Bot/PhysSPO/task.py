12cef73af4af5a486a7058d177915fb619449de6
Юрий Солдатов <141447194+general-soldatov@users.noreply.github.com>
2024-02-26 12:08:24 +0300

    Update task.py

--- Telegram Bot/PhysSPO/task.py
+++ Telegram Bot/PhysSPO/task.py
@@ -2,3 +2,18 @@ from random import uniform
 from math import cos, sin, pi, atan
 rad = lambda alpha: alpha * pi / 180
 
+def task_9() -> str | float | str:
+    URL = True
+    n = round(uniform(2.1, 500.1), 1)
+    F1 = round(uniform(2.1, 500.1), 1)
+    F2 = round(uniform(2.1, 500.1), 1)
+    F3 = round(uniform(2.1, 500.1), 1)
+    alpha = round(uniform(5.2, 60.7), 1)
+    text = f'''Определить модуль равнодействующей.
+F1 = {F1} Н, F2 = {F2} Н, F3 = {F3} Н, ​α = {alpha}°.
+Модуль R, Н округлить до десятых.
+    '''
+    result = F2 + F1 + F3 * cos(alpha)
+    return text, round(result, 1), URL
+
+print(task_9())
